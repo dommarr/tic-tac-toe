844da584d421ce150411040f3bad208926cfa41c
dommarr <dom.marrone@gmail.com>
2019-06-27 10:23:59 -0400

    Create turn logic and new game
    Created turn logic and new game button to reset board.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,11 +7,41 @@
 // require('./example')
 
 const authEvents = require('./auth/events')
-//const exampleEvents = require('./examples/events')
+// const exampleEvents = require('./examples/events')
+
+let turnSuccess = 'n'
+let lastTurn = 'o'
+// let game = []
+// const winner = () => {
+//
+// }
 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('click', authEvents.onSignOut)
+})
+
+// Turn Logic
+$('.board').on('click', function (event) {
+  let target = $(event.target)
+  // Check for empty square
+  if (target.is(':empty')) {
+    // If empty & last turn was O, place X
+    if (lastTurn === 'o') {
+      target.text('X')
+      turnSuccess = 'y'
+      lastTurn = 'x'
+      // If empty & last turn was X, place O
+    } else if (lastTurn === 'x') {
+      target.text('O')
+      turnSuccess = 'y'
+      lastTurn = 'o'
+    }
+  } // Need to add else for if not empty, then give alert/modal.
+})
+
+$('#new-game').on('click', function (event) {
+  $('.board').empty()
 })
